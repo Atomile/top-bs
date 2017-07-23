d3370164e96ace60f9d23d111071c066ac3efb1f
liuwanlong <flyinglong@outlook.com>
2017-07-23 21:18:00 +0800

    添加路由

--- routes/index.js
+++ routes/index.js
@@ -35,21 +35,41 @@ router.get('/getnews/:type', function (req, res, next) {
 });
 //发表评论
 router.post('/topic', function (req, res, next) {
-    console.log(req.body);
     let sql = 'insert into topic set ?';
     mysql.query(sql, req.body, (err, result) => {
         if (!err) {
-            return res.send({
-                success: true
+            let sql = 'update news set r_count=r_count+1 where news_id=?';
+            mysql.query(sql, req.body.news_id, (err, result) => {
+                if (!err) {
+                    return res.send({
+                        success: true
+                    })
+                } else {
+                    console.log(err);
+                }
             })
         } else {
             console.log(err);
         }
     })
 });
+//新闻观看次数+1
+router.post('/view', function (req, res, next)  {
+    let sql = 'update news set v_count=v_count+1 where news_id = ?';
+    mysql.query(sql, req.body.id, (err, result) => {
+        if (!err) {
+            return res.send({
+                success: true
+            })
+        } else {
+            return res.send({
+                success: false
+            })
+        }
+    })
+});
 //评论回复数(热度)加1
 router.post('/topic/add_rply', function (req, res, next) {
-    console.log(req.body.id);
     let sql = 'update topic set rp_count=rp_count+1 where t_id=?';
     mysql.query(sql, req.body.id, (err, result) => {
         if (!err) {
@@ -63,7 +83,6 @@ router.post('/topic/add_rply', function (req, res, next) {
 });
 //评论点赞
 router.post('/topic/vote', function (req, res, next) {
-    console.log(req.body);
     let sql = '';
     if (req.body.type) {
         sql = 'update topic set vote_up=vote_up+1 where t_id=?';
@@ -72,7 +91,6 @@ router.post('/topic/vote', function (req, res, next) {
     }
     mysql.query(sql, req.body.id, function (err, result) {
         if (!err) {
-            console.log(result);
             return res.send({success: true})
         } else {
             console.log(err);
@@ -96,7 +114,6 @@ router.get('/topic/children/:t_id', function (req, res, next) {
     let sql = 'select * from topic where t_to_t=?';
     mysql.query(sql, req.params.t_id, (err, results) => {
         if (!err) {
-            console.log(results);
             return res.send({data: results});
         } else {
             console.log(err);
@@ -108,7 +125,6 @@ router.get('/topic/children/:t_id', function (req, res, next) {
 /// ************ shan shu shi pin guang gao*************
 router.get('/video/:number', function (req, res) {
     var c = req.params.number;
-    console.log(c);
     var options = {
         hostname: 'newapi.meipai.com',
         path: '/output/channels_topics_timeline.json?id=1&count=' + c,
